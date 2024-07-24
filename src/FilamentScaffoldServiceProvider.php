71e0c2954cccc79d65006c7b8b24cdfd684be796
celia514 <hong-ching.chung@outlook.com>
2024-07-24 15:50:14 +0800

    no message

--- src/FilamentScaffoldServiceProvider.php
+++ src/FilamentScaffoldServiceProvider.php
@@ -2,8 +2,12 @@
 
 namespace Solutionforest\FilamentScaffold;
 
+use Filament\Support\Assets\Asset;
+use Filament\Support\Facades\FilamentAsset;
+use Livewire\Features\SupportTesting\Testable;
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
+use Solutionforest\FilamentScaffold\Testing\TestsFilamentScaffold;
 
 class FilamentScaffoldServiceProvider extends PackageServiceProvider
 {
@@ -14,5 +18,24 @@ class FilamentScaffoldServiceProvider extends PackageServiceProvider
     public function configurePackage(Package $package): void
     {
         $package->name(static::$name);
+    }
+
+    public function packageRegistered(): void {}
+
+    public function packageBooted(): void
+    {
+        // Asset Registration
+        FilamentAsset::register(
+            $this->getAssets(),
+            $this->getAssetPackageName()
+        );
+
+        // Testing
+        Testable::mixin(new TestsFilamentScaffold());
+    }
+
+    protected function getAssetPackageName(): ?string
+    {
+        return 'solution-forest/filament-scaffold';
     }
 }
---
+++ src/Testing/TestsFilamentScaffold.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Solutionforest\FilamentScaffold\Testing;
+
+use Livewire\Features\SupportTesting\Testable;
+
+/**
+ * @mixin Testable
+ */
+class TestsFilamentScaffold
+{
+    //
+}
